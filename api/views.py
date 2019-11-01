28256f87b02ea982829ab7fe46b6366b87d7242f
GreicePaz <greicepazvargas@gmail.com>
2019-11-01 20:24:30 -0300

    Alteração do método de criação de user, para criar o token junto com a criação. Retirada da url de criação de token.

--- api/views.py
+++ api/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
 from api.models import Log, User
 from api.serializers import LogsModelSerializer, UserModelSerializer
 
-# Create your views here.
 
 class LogsAPIView(APIView): 
 
@@ -52,6 +50,13 @@ class UserAPIView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            instance = serializer.save()
+
+            #Criação Token do usuário
+            token = Token.objects.create(user=instance)
+            
+            response = serializer.data
+            response['token'] = token.key 
+
+            return Response(response, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
